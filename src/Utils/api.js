c3bc227d21ff5ee8b6240edbab38bad67e279197
jomit1612 <jomitthomas1612@gmail.com>
2022-06-17 11:02:39 +0100

    delete functionality added

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -55,3 +55,9 @@ export const postComment = (article_id, username, body) => {
       return res.data;
     });
 };
+
+export const deleteComment = (comment_id) => {
+  return articlesApi.delete(`/comments/${comment_id}`).then((res) => {
+    return res.data;
+  });
+};
--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getComments } from "../Utils/api";
 import { useParams } from "react-router-dom";
+import { deleteComment } from "../Utils/api";
 
 const ViewComments = () => {
   const [allComments, setAllComments] = useState([]);
@@ -12,6 +13,10 @@ const ViewComments = () => {
     });
   }, [article_id]);
 
+  const removeComment = (comment_id) => {
+    deleteComment(comment_id).then(setAllComments(allComments));
+  };
+
   return (
     <>
       <section className="Ma">
@@ -22,6 +27,14 @@ const ViewComments = () => {
                 <h3>Username:{comment.author}</h3>
                 <h3>Comment:{comment.body}</h3>
                 <h3>Votes:{comment.votes}</h3>
+                <button
+                  className="delete"
+                  onClick={() => {
+                    removeComment(comment.comment_id);
+                  }}
+                >
+                  Delete Comment
+                </button>
               </li>
             );
           })}
